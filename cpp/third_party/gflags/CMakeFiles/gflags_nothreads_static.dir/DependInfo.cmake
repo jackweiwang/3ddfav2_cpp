1e75bd302ab6a6a856bdfc9a236987636689f620
jackweiwang <454940304@qq.com>
2021-08-06 09:18:35 +0800

    release v1.0

---
+++ cpp/third_party/gflags/CMakeFiles/gflags_nothreads_static.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ww/google/3ddfav2_cpp/cpp/third_party/gflags/src/gflags.cc" "/home/ww/google/3ddfav2_cpp/cpp/third_party/gflags/CMakeFiles/gflags_nothreads_static.dir/src/gflags.cc.o"
+  "/home/ww/google/3ddfav2_cpp/cpp/third_party/gflags/src/gflags_completions.cc" "/home/ww/google/3ddfav2_cpp/cpp/third_party/gflags/CMakeFiles/gflags_nothreads_static.dir/src/gflags_completions.cc.o"
+  "/home/ww/google/3ddfav2_cpp/cpp/third_party/gflags/src/gflags_reporting.cc" "/home/ww/google/3ddfav2_cpp/cpp/third_party/gflags/CMakeFiles/gflags_nothreads_static.dir/src/gflags_reporting.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  "NO_THREADS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ww/google/3ddfav2_cpp/cpp/tnn_demo/../third_party/TNN/include"
+  "/home/ww/google/3ddfav2_cpp/cpp/tnn_demo/../third_party/stb"
+  "/home/ww/google/3ddfav2_cpp/cpp/tnn_demo/base"
+  "/home/ww/google/3ddfav2_cpp/cpp/tnn_demo/utils"
+  "/home/ww/google/3ddfav2_cpp/cpp/third_party/gflags/include"
+  "/home/ww/google/3ddfav2_cpp/cpp/third_party/gflags/src"
+  "/home/ww/google/3ddfav2_cpp/cpp/third_party/gflags/include/gflags"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
